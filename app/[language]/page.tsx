1ffcf377bff1ad91045d3d2b7a6c1bd6f577ee42
CarlesRojas <carlesrojas@outlook.com>
2024-05-12 15:26:29 +0200

    fix build

--- app/[language]/page.tsx
+++ app/[language]/page.tsx
@@ -1,6 +1,7 @@
 import { PageProps } from "@/app/[language]/layout";
 import SpellList from "@/component/SpellList";
 import { getAllSpells } from "@/server/repo/spell";
+import { Suspense } from "react";
 
 export const revalidate = 60 * 60 * 24; // 1 day
 
@@ -10,7 +11,9 @@ const Spells = async ({ params: { language } }: PageProps) => {
     return (
         <main className="relative flex h-full w-full flex-col items-center">
             <div className="relative flex h-fit min-h-full w-full justify-center overflow-y-auto">
-                <SpellList language={language} initialSpellsData={initialSpellsData} />
+                <Suspense fallback={null}>
+                    <SpellList language={language} initialSpellsData={initialSpellsData} />
+                </Suspense>
             </div>
         </main>
     );
