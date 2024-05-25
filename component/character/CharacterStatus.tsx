b301f6dfe1d0bffda0798fba6fe912105d4aeaaa
CarlesRojas <carlesrojas@outlook.com>
2024-05-25 11:54:05 +0200

    add button

--- component/character/CharacterStatus.tsx
+++ component/character/CharacterStatus.tsx
@@ -1,11 +1,12 @@
 import { getClassIcon } from "@/component/character/CharacterItem";
 import { Button } from "@/component/ui/button";
 import { useTranslation } from "@/hook/useTranslation";
-import { getAbility, getClassBackgroundColor, getClassColorOnHover, getTotalSpellSlots } from "@/lib/character";
+import { getAbility, getClassBackgroundColor, getTotalSpellSlots } from "@/lib/character";
 import { cn } from "@/lib/util";
 import { CharacterWithSpells } from "@/type/Character";
 import { Language } from "@/type/Language";
 import { LuPencil } from "react-icons/lu";
+import { PiCampfireDuotone } from "react-icons/pi";
 
 interface CommonProps {
     language: Language;
@@ -32,14 +33,14 @@ const CharacterStatus = (props: Props) => {
     const { id, name, class: characterClass, level, ability, spellSlotsAvailable } = props.character;
 
     return (
-        <div className="fixed flex w-full max-w-screen-lg flex-col items-center gap-4 rounded border-b border-stone-300 bg-stone-100 p-3 dark:border-stone-700 dark:bg-stone-950 sm:justify-between sm:p-4 md:flex-row mouse:top-16">
-            <div className="relative flex w-full items-center justify-between gap-6 md:w-fit md:justify-normal">
-                <div className="flex items-center gap-3">
-                    {getClassIcon(characterClass, "h-9 min-h-9 w-9 -my-2 -mx-1")}
+        <div className="fixed flex w-full max-w-screen-lg flex-col items-center gap-2 border-b border-stone-300 bg-stone-100 p-3 dark:border-stone-700 dark:bg-stone-950 sm:flex-row sm:justify-between sm:gap-4 sm:border-none sm:p-4 mouse:top-16">
+            <div className="relative flex w-full items-center gap-2 sm:w-fit">
+                {getClassIcon(characterClass, "h-12 min-h-12 w-12 -my-2")}
+
+                <div className="flex w-fit grow flex-col sm:grow-0">
+                    <h3 className="truncate font-semibold">{name}</h3>
 
                     <div className="flex items-baseline gap-3 sm:gap-4">
-                        <h3 className={`font-semibold ${getClassColorOnHover(characterClass)}`}>{name}</h3>
-
                         <span>
                             <span className="font-medium opacity-50">{t.dnd.level}</span>{" "}
                             <strong className="font-semibold">{level}</strong>
@@ -54,12 +55,12 @@ const CharacterStatus = (props: Props) => {
                     </div>
                 </div>
 
-                <Button variant="outline" size="icon" className="-mx-2 -my-2">
+                <Button variant="outline" size="icon" className="sm:ml-4">
                     <LuPencil className="h-4 w-4" />
                 </Button>
             </div>
 
-            <div className="relative flex w-full items-start justify-between sm:gap-x-4 md:w-fit md:justify-normal lg:gap-x-6">
+            <div className="relative flex h-fit w-full items-center justify-between sm:w-fit sm:justify-normal sm:gap-x-4 lg:gap-x-6">
                 {spellSlotsAvailable &&
                     Object.keys(spellSlotsAvailable)
                         .sort()
@@ -70,23 +71,26 @@ const CharacterStatus = (props: Props) => {
                             const availableSpellSlots =
                                 spellSlotsAvailable[slotLevel as keyof typeof spellSlotsAvailable];
 
-                            if (maxSpellSlots === 0) return null;
+                            if (maxSpellSlots === 0)
+                                return <div key={slotLevel} className="w-7 min-w-7 max-w-7 sm:hidden" />;
 
                             return (
                                 <div
                                     key={slotLevel}
-                                    className="relative flex w-7 min-w-7 max-w-7 flex-col items-center justify-center rounded-md text-sm font-semibold"
+                                    className="relative flex h-full w-7 min-w-7 max-w-7 flex-col items-center justify-center rounded-md text-sm font-semibold"
                                 >
-                                    {slotLevel.replace(/\D/g, "")}
+                                    <p>{slotLevel.replace(/\D/g, "")}</p>
 
                                     <div className="flex w-full flex-wrap items-center justify-center gap-1">
-                                        {Array.from({ length: maxSpellSlots }).map((_, index) => (
+                                        {Array.from({ length: Math.max(maxSpellSlots, 3) }).map((_, index) => (
                                             <div
                                                 key={index}
                                                 className={cn(
                                                     "h-3 max-h-3 min-h-3 w-3 min-w-3 max-w-3 rounded-full bg-stone-500/30",
                                                     index < availableSpellSlots &&
                                                         getClassBackgroundColor(characterClass),
+                                                    index > maxSpellSlots &&
+                                                        "pointer-events-none select-none opacity-0",
                                                 )}
                                             />
                                         ))}
@@ -94,6 +98,12 @@ const CharacterStatus = (props: Props) => {
                                 </div>
                             );
                         })}
+
+                <div className="relative flex h-full w-fit items-center justify-center ">
+                    <Button variant="outline" size="icon" className="">
+                        <PiCampfireDuotone className="h-6 w-6" />
+                    </Button>
+                </div>
             </div>
         </div>
     );
