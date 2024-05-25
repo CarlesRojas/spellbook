7e4571199921517835213f5aa2e35eba35fed738
CarlesRojas <carlesrojas@outlook.com>
2024-05-25 12:03:12 +0200

    fix responsiveness

--- component/character/CharacterStatus.tsx
+++ component/character/CharacterStatus.tsx
@@ -33,11 +33,11 @@ const CharacterStatus = (props: Props) => {
     const { id, name, class: characterClass, level, ability, spellSlotsAvailable } = props.character;
 
     return (
-        <div className="fixed flex w-full max-w-screen-lg flex-col items-center gap-2 border-b border-stone-300 bg-stone-100 p-3 dark:border-stone-700 dark:bg-stone-950 sm:flex-row sm:justify-between sm:gap-4 sm:border-none sm:p-4 mouse:top-16">
-            <div className="relative flex w-full items-center gap-2 sm:w-fit">
+        <div className="fixed flex w-full max-w-screen-lg flex-col items-center gap-2 bg-stone-100 p-3 dark:bg-stone-950 sm:p-4 md:flex-row md:justify-between md:gap-4 mouse:top-16">
+            <div className="relative flex w-full items-center gap-2 md:w-fit">
                 {getClassIcon(characterClass, "h-12 min-h-12 w-12 -my-2")}
 
-                <div className="flex w-fit grow flex-col sm:grow-0">
+                <div className="flex w-fit grow flex-col md:grow-0">
                     <h3 className="truncate font-semibold">{name}</h3>
 
                     <div className="flex items-baseline gap-3 sm:gap-4">
@@ -58,9 +58,13 @@ const CharacterStatus = (props: Props) => {
                 <Button variant="outline" size="icon" className="sm:ml-4">
                     <LuPencil className="h-4 w-4" />
                 </Button>
+
+                <Button variant="outline" size="icon" className="">
+                    <PiCampfireDuotone className="h-6 w-6" />
+                </Button>
             </div>
 
-            <div className="relative flex h-fit w-full items-center justify-between sm:w-fit sm:justify-normal sm:gap-x-4 lg:gap-x-6">
+            <div className="relative flex h-fit w-full min-w-fit items-start justify-between sm:gap-x-4 md:w-fit md:justify-end lg:gap-x-6">
                 {spellSlotsAvailable &&
                     Object.keys(spellSlotsAvailable)
                         .sort()
@@ -72,7 +76,7 @@ const CharacterStatus = (props: Props) => {
                                 spellSlotsAvailable[slotLevel as keyof typeof spellSlotsAvailable];
 
                             if (maxSpellSlots === 0)
-                                return <div key={slotLevel} className="w-7 min-w-7 max-w-7 sm:hidden" />;
+                                return <div key={slotLevel} className="w-7 min-w-7 max-w-7 md:hidden" />;
 
                             return (
                                 <div
@@ -82,15 +86,13 @@ const CharacterStatus = (props: Props) => {
                                     <p>{slotLevel.replace(/\D/g, "")}</p>
 
                                     <div className="flex w-full flex-wrap items-center justify-center gap-1">
-                                        {Array.from({ length: Math.max(maxSpellSlots, 3) }).map((_, index) => (
+                                        {Array.from({ length: maxSpellSlots }).map((_, index) => (
                                             <div
                                                 key={index}
                                                 className={cn(
                                                     "h-3 max-h-3 min-h-3 w-3 min-w-3 max-w-3 rounded-full bg-stone-500/30",
                                                     index < availableSpellSlots &&
                                                         getClassBackgroundColor(characterClass),
-                                                    index > maxSpellSlots &&
-                                                        "pointer-events-none select-none opacity-0",
                                                 )}
                                             />
                                         ))}
@@ -98,12 +100,6 @@ const CharacterStatus = (props: Props) => {
                                 </div>
                             );
                         })}
-
-                <div className="relative flex h-full w-fit items-center justify-center ">
-                    <Button variant="outline" size="icon" className="">
-                        <PiCampfireDuotone className="h-6 w-6" />
-                    </Button>
-                </div>
             </div>
         </div>
     );
