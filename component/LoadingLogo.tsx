edc03178ca24daa8dac8dfaa45467eb6aab77866
CarlesRojas <carlesrojas@outlook.com>
2024-05-26 18:22:25 +0200

    pretty

--- component/LoadingLogo.tsx
+++ component/LoadingLogo.tsx
@@ -3,7 +3,7 @@
 import D20 from "@/asset/dice/D20Loader.svg";
 
 const LoadingLogo = () => {
-    return <D20 className="animate-slow-spin h-16 w-16 opacity-60" />;
+    return <D20 className="h-16 w-16 animate-slow-spin opacity-60" />;
 };
 
 export default LoadingLogo;
